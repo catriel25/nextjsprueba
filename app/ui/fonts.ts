4337e438be1688879399357177e08c42e1cc1c9c
catriel25 <catrielconde@gmail.com>
2024-05-12 16:39:39 -0300

    cambie el tema de las fonts y saque bold y regular

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -4,5 +4,5 @@ import { Lusitana } from 'next/font/google';
 export const inter = Inter({ subsets: ['latin'] });
 
 // Especificar los pesos disponibles para la fuente Lusitana
-export const lusitanaRegular = Lusitana({ subsets: ['latin'], weight: '400' });
-export const lusitanaBold = Lusitana({ subsets: ['latin'], weight: '700' });
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '700' });
